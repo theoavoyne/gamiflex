30fc6d52b6a2e446acee84740bc91303ee325dee
Theo Avoyne <tavoyne@gmail.com>
2018-03-09 13:59:21 -0500

    fixed bug home controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
         @suggestion = nil if @suggestion == []
       elsif policy(current_user).choose_games?
         games_played = current_user.states.map do |element|
-          element.game_id
+          element.game_id.to_s
         end
         game_list = %w(233 987 559 1628 6036 8173 533 1942 1020 20 239 127 501 72 7331 974 27081 1905 7360 121 2691 529 12517 11198 4754 36872 1331 2350  9061 434)
         game_list = game_list - games_played
