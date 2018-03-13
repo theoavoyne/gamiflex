66584e33486a7d8fcc87a229bb08146f30b7e4f7
Nicolas Fan <nicfan@gmail.com>
2018-03-13 16:38:21 -0400

    buttons and animations added to the carousel

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,10 +1,25 @@
 class StatesController < ApplicationController
   def create
     @state = current_user.states.build(state_params)
+    @game_id = params[:game_id]
     @state.user = current_user
+
     authorize @state
-    @state.save
-    # redirect_to params[:redirect] ? root_path : game_path(@state.game)
+
+    if @state.save
+      if params[:state] == "like"
+        @remaining = 5 - current_user.states.where(state: 'like').length
+      end
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render "/"}
+        format.js
+      end
+    end
   end
 
   def destroy
--- app/javascript/carousel/carousel.js
+++ app/javascript/carousel/carousel.js
@@ -11,17 +11,26 @@ $('.slider').slick({
   nextArrow: '#prevArrow',
   slidesToShow: 6,
   slidesToScroll: 1,
-  inifinite: true,
+  infinite: true,
   speed: 1000,
   responsive: [
     {
-      breakpoint: 1200,
+      breakpoint: 900,
       settings: {
         slidesToShow: 4,
         slidesToScroll: 1,
         infinite: true,
+        speed: 1000
       }
     },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    }
   ]
 });
 
@@ -32,17 +41,13 @@ function sideImages() {
   const active = document.querySelectorAll('.slick-active');
   active.forEach(function(card) {
     if( card == active[active.length - 1] || card == active[0] ) {
+      card.style.transition = 'all 1s';
       card.style.opacity = '0.2';
-      card.querySelector('.item-carousel').style.width = '80%';
-      card.querySelector('.item-carousel').style.height = '30vh'; // added
       card.querySelector('.item-carousel').style.margin = '0 auto';
       card.style.paddingTop = '10px'; // modified
       card.style.paddingBottom = '10px'; // modified
-      card.style.transition = 'all 1s';
     } else {
       card.style.opacity = '1';
-      card.querySelector('.item-carousel').style.width = '100%';
-      card.querySelector('.item-carousel').style.height = '35vh'; // added
       card.querySelector('.item-carousel').style.margin = '0';
       card.style.paddingTop = '0';
     }
@@ -64,11 +69,7 @@ if(window.innerWidth < 1200) {
 }
 
 
-// Game card disappears when user clicks 'Like' or 'Dislike' //
-const likeBtn = document.querySelector('.like-btn');
-console.log(likeBtn);
-const dislikeBtn = document.querySelector('.dislike-btn');
-
+// 'Help' Popover
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
