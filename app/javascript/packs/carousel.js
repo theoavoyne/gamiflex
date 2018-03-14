3227be662b75f20a26cbbf9478729ec0a05da1ed
Olivier Riccini <olivier-webdev@hotmail.com>
2018-03-14 18:19:29 -0400

    carousel fixed

--- app/javascript/packs/carousel.js
+++ app/javascript/packs/carousel.js
@@ -1,12 +1,9 @@
 import "slick-carousel";
 
-const cards = document.querySelectorAll('.item-carousel');
 ////////////////////
-sideImages();
 /* Implement Slick function */
 $('.slider').slick({
-  prevArrow: '.rightClickable',
-  nextArrow: '.leftClickable',
+  arrows: false,
   slidesToShow: 6,
   slidesToScroll: 1,
   infinite: true,
@@ -35,6 +32,9 @@ $('.slider').slick({
 /* Apply style to side images*/
 function sideImages() {
   const active = document.querySelectorAll('.slick-active');
+  const makeClickable = document.querySelectorAll('.slick-active .item-carousel');
+  makeClickable[0].insertAdjacentHTML('afterbegin', `<div class="clickable leftClickable"></div>`);
+  makeClickable[makeClickable.length - 1].insertAdjacentHTML('afterbegin', `<div class="clickable rightClickable"></div>`);
 
   active.forEach(function(card) {
     if( card == active[active.length - 1] || card == active[0] ) {
@@ -43,64 +43,37 @@ function sideImages() {
       card.querySelector('.item-carousel').style.margin = '0 auto';
       card.style.paddingTop = '10px'; // modified
       card.style.paddingBottom = '10px'; // modified
-      let makeClickable = document.querySelectorAll('.slick-active .non-clickable');
-      makeClickable[0].style.backgroundColor = `blue`;
-      makeClickable[0].classList.add('leftClickable');
-      makeClickable[makeClickable.length - 1].style.backgroundColor = `green`;
-      makeClickable[makeClickable.length - 1].classList.add('rightClickable');
+
     } else {
       card.style.opacity = '1';
       card.querySelector('.item-carousel').style.margin = '0';
       card.style.paddingTop = '0';
     }
   });
+
+  $('.leftClickable').click(function(){
+      $('.slider').slick('slickPrev');
+      $('div').remove('.clickable');
+      sideImages();
+    })
+
+  $('.rightClickable').click(function(){
+      $('.slider').slick('slickNext');
+      $('div').remove('.clickable');
+      sideImages();
+  })
 }
 
 sideImages();
 
-// function spanClickResize() {
-//   const active = document.querySelectorAll('.slick-active img');
-//   var rectLeft = active[0].getBoundingClientRect();
-//   var rectRight = active[active.length - 1].getBoundingClientRect();
-
-//   left.style.width = `${active[0].offsetWidth}px`;
-//   right.style.width = `${active[active.length - 1].offsetWidth}px`;
-//   left.style.height = `${active[0].offsetHeight}px`;
-//   right.style.height = `${active[active.length - 1].offsetHeight}px`;
-
-//   left.style.bottom = `calc(100vh - ${rectRight.bottom}px)`;
-//   right.style.bottom = `calc(100vh - ${rectLeft.bottom}px)`;
-//   left.style.left = `${rectLeft.left}px`;
-//   right.style.left = `${rectRight.left}px`;
-
-//   left.style.backgroundColor = `blue`;
-//   right.style.backgroundColor = `red`;
-
-// }
-
-// spanClickResize();
-const left = document.querySelector('.leftClickable');
-const right = document.querySelector('.rightClickable');
-
-////////////////////
+///////////////////
 
 window.addEventListener('resize', function(){
   sideImages();
 });
 
-left.addEventListener('click', function() {
-  console.log('left');
-  sideImages();
-  // $('.slider').slick({
-  //   slickGoTo: 1
-  // });
-});
+/////////////////////
 
-right.addEventListener('click', function() {
-  console.log('right');
-  sideImages();
-});
-////////////////////
 if(window.innerWidth < 1200) {
   sideImages();
 }
