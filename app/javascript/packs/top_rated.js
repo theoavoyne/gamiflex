992d6fd0297c8c084b773dd4bc827c05a8676c6d
Olivier Riccini <olivier-webdev@hotmail.com>
2018-03-14 20:13:40 -0400

    finito

--- app/javascript/packs/top_rated.js
+++ app/javascript/packs/top_rated.js
@@ -3,5 +3,22 @@ console.log("hello")
 import "slick-carousel";
 
 $(document).ready(function(){
-  $('').slick();
+  $('.autoplay-top-rated').slick({
+    arrows: false,
+    slidesToShow: 3,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+  });
+  $('.autoplay-currently-consulted').slick({
+    arrows: false,
+    slidesToShow: 3,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    variableWidth: true,
+  });
 });
