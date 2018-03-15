2f2e86f4870da1763b952d0b16fca46ec273c73e
Theo Avoyne <tavoyne@gmail.com>
2018-03-15 17:20:17 -0400

    edit game list" é

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -34,7 +34,7 @@ class PagesController < ApplicationController
   def choose
     if policy(current_user).choose_games?
       games_played = current_user.states.pluck(:game_id).map { |game_id| game_id.to_s }
-      game_list = %w(233 987 559 1628 6036 8173 533 1942 1020 20 239 127 501 72 7331 974 27081 1905 7360 121 2691 529 12517 11198 4754 36872 1331 2350  9061 434)
+      game_list = %w(434 233 987 559 1628 6036 8173 533 1942 1020 20 239 127 501 72 7331 974 27081 1905 7360 121 2691 529 12517 11198 4754 36872 1331 2350  9061 434)
       game_list = game_list - games_played
       @games = game_list.map do |game|
         Game.find_with_igdb(game)
